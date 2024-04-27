cce5a93a890286c2d92db7e70b5bd08265670071
EzeQuezada <20231021.itla.edu.do>
2024-04-27 16:15:31 -0400

    Agregar archivos de proyecto.

---
+++ Person.cs
@@ -0,0 +1,26 @@
+﻿
+
+namespace Person
+{
+    public class Persona
+    {
+        private string? nombre;
+        private int edad;
+        private string? dirección;
+
+        public void EnviarCorreoElectronico(string destinatario, string mensaje)
+        {
+            // Lógica para enviar el correo electrónico
+            Console.WriteLine($"Enviando correo a: {destinatario}");
+            Console.WriteLine($"Mensaje: {mensaje}");
+        }
+
+        public void ImprimirEnConsola()
+        {
+            // Lógica para imprimir información en la consola
+            Console.WriteLine($"Nombre: {nombre}");
+            Console.WriteLine($"Edad: {edad} años");
+            Console.WriteLine($"Dirección: {dirección}");
+        }
+    }  
+}
